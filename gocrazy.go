6143074627f76752b6af69de8d69a76f76129d56
Levi Richardson <litbelb1@outlook.com>
2021-03-01 13:38:17 -0800

    Update gocrazy.go

--- gocrazy.go
+++ gocrazy.go
@@ -1,32 +1,32 @@
 package main
 
 import (
-	"fmt"
-	"math"
-	"strings"
+        "fmt"
+        "math"
+        "strings"
 
-	"syreclabs.com/go/faker"
+        "syreclabs.com/go/faker"
 )
 
 func rgb(i int) (int, int, int) {
-	var f = 0.1
-	return int(math.Sin(f*float64(i)+0)*127 + 128),
-		int(math.Sin(f*float64(i)+2*math.Pi/3)*127 + 128),
-		int(math.Sin(f*float64(i)+4*math.Pi/3)*127 + 128)
+        var f = 0.1
+        return int(math.Sin(f*float64(i)+0)*127 + 128),
+                int(math.Sin(f*float64(i)+2*math.Pi/3)*127 + 128),
+                int(math.Sin(f*float64(i)+4*math.Pi/3)*127 + 128)
 }
 
 func main() {
-	var phrases []string
+        var phrases []string
 
-	for i := 1; i < 3; i++ {
-		phrases = append(phrases, faker.Hacker().Phrases()...)
-	}
+        for i := 1; i < 3; i++ {
+                phrases = append(phrases, faker.Hacker().Phrases()...)
+        }
 
-	output := strings.Join(phrases[:], "; ")
+        output := strings.Join(phrases[:], "; ")
 
-	for j := 0; j < len(output); j++ {
-		r, g, b := rgb(j)
-		fmt.Printf("\033[38;2;%d;%d;%dm%c\033[0m", r, g, b, output[j])
-	}
-	fmt.Println()
+        for j := 0; j < len(output); j++ {
+                r, g, b := rgb(j)
+                fmt.Printf("\033[38;2;%d;%d;%dm%c\033[0m", r, g, b, output[j])
+        }
+        fmt.Println()
 }
